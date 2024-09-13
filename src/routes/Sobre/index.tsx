89745eedf6c1472080539ac4c194b4d87771862e
GustavoSMelo <gsmelo2804@gmail.com>
2024-09-12 22:47:13 -0300

    Finalizando a pagina sobre

--- src/routes/Sobre/index.tsx
+++ src/routes/Sobre/index.tsx
@@ -24,15 +24,34 @@ export default function Sobre(){
             </nav>
             <button className="cta-button" onClick={handleLoginClick}>Fazer login</button>
             </header>
-            <section className='sec-sobre'>
+            <div className='div-Sobre'>
 
-                <div className='Devs-so'>
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
+                <div className='dsec-sobre'>
+                <section className='sec-sobre'>
+                    <h1>CarDoctor</h1>
+                </section>
+
                 </div>
-            </section>
-
+            </div>
+            <footer className='footer_Sobre'>
+                <div className='participantes'>
+                    <div className='gusta'>
+                        <img src="shrek 3.png" alt="" />
+                        <p>Gustavo Souto </p>
+                        <p><img src="git_icon.png" alt="" />RM558595</p>
+                    </div>
+                    <div className='miche'>
+                        <img src="shrek 2.png" alt="" />
+                        <p>Michele Souza</p>
+                        <p><img src="git_icon.png" alt="" />RM558596</p>
+                    </div>
+                    <div className='luiza'>
+                        <img src="shrek 4.png" alt="" />
+                        <p>Luiza Danielle</p>
+                        <p><img src="git_icon.png" alt="" />RM558597</p>
+                    </div>
+                </div>
+            </footer>
             
 
         </main>
